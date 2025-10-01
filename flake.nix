3506ef8f6c1d51d132580f2c889e83bce0de3234
rooyca <rooyca@gmail.com>
2025-10-01 14:43:00 -0500

    update(add): barli rust version

--- flake.nix
+++ flake.nix
@@ -1,7 +1,3 @@
-
-
-
-
 {
   description = "ron's meta-package flake";
 
@@ -10,11 +6,13 @@
 
     barli.url = "github:rooyca/barli-c";
     barli.inputs.nixpkgs.follows = "nixpkgs"; 
+    barli-rust.url = "github:rooyca/barli";
+    barli-rust.inputs.nixpkgs.follows = "nixpkgs"; 
     minipm.url = "github:rooyca/minipm";
     minipm.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, barli, minipm, ... }:
+  outputs = { self, nixpkgs, barli, barli-rust, minipm, ... }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in {
@@ -23,6 +21,7 @@
           pkgs = import nixpkgs { inherit system; };
         in {
           barli = barli.packages.${system};
+          barli-rust = barli-rust.packages.${system}.default;
           minipm = minipm.packages.${system}.default;
         }
       );
